194ca39cc2f94e3ed4ba9d642d547d6c17b1c11a
Madison Lawton <madison.e.lawton@gmail.com>
2019-04-22 15:31:24 -0400

    updating for current projects

--- js/drop-down.js
+++
@@ -1,9 +0,0 @@
-function toggleAboutMe(element) {
-    element.classList.toggle('hide')
-}
-function toggleAboutMeOnClick(element) {
-    element.addEventListener('click', function () {
-        toggleAboutMe(this.parentElement.nextElementSibling.nextElementSibling.nextElementSibling)
-    })
-}
-toggleAboutMeOnClick(document.querySelector('.toggle-button'))
